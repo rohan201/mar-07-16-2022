65c72c701f745ae5db6320855bce1530e9257899
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-03-08 15:59:38 +0530

    Day02

---
+++ day02-app/src/Footer.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return <h3>Copyrights reserved, 2022</h3>;
+    }
+}
+
+export default Footer;
---
+++ day02-app/src/Header.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return <h1>Hello Header</h1>;
+    }
+}
+
+export default Header;
