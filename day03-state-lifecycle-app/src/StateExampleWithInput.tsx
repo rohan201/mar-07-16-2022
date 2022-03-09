85c9f8e0f1bf19119a5edd5f1474bf89cd725a03
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-03-09 15:03:13 +0530

    Day03

--- day03-state-lifecycle-app/src/StateExampleWithInput.tsx
+++ day03-state-lifecycle-app/src/StateExampleWithInput.tsx
@@ -3,20 +3,56 @@ import { Component } from "react";
 interface IStateExample {
     title: string;
     message: string;
+    name: string;
+    subscribedToWhatsAppUpdates: boolean
 }
 class StateExampleWithInput extends Component<{}, IStateExample> {
     constructor(props: {}) {
         super(props);
         this.state = {
             title: 'State example with input',
-            message: ''
+            message: '',
+            name: '',
+            subscribedToWhatsAppUpdates: false
         };
+        this.nameChanged = this.nameChanged.bind(this); //WILL EXPLAIN LATER
+        this.buttonClicked = this.buttonClicked.bind(this); //WILL EXPLAIN LATER
+        this.waUpdateChanged = this.waUpdateChanged.bind(this); //WILL EXPLAIN LATER
     }
+
+    waUpdateChanged(e: any) {
+        let value = e.currentTarget.checked;
+        this.setState({
+            subscribedToWhatsAppUpdates: value
+        });
+    }
+
+    buttonClicked() {
+        let waSubscription = this.state.subscribedToWhatsAppUpdates;
+        let message = `Hi ${this.state.name}!`;
+        if(waSubscription) {
+            message += ' You will receive updates on WhatsApp';
+        }
+        this.setState({
+            //message: message
+            message
+        });
+    }
+
+    nameChanged(e: any) {
+        console.log(e);
+        let currentValue = e.currentTarget.value;
+        this.setState({
+            name: currentValue
+        });
+    }
+
     render() {
         return (<div>
             <h1>{this.state.title}</h1>
-            <input type="text" placeholder="Name"></input> <br/>
-            <button>Click</button>
+            <input type="text" placeholder="Name" onChange={this.nameChanged}></input> <br/>
+            <input type="checkbox" onChange={this.waUpdateChanged}></input><label>You want receive WhatsApp updates</label> <br/><br/>
+            <button onClick={this.buttonClicked}>Click</button>
             <h3>{this.state.message}</h3>
         </div>);
     }
