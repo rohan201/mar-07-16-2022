3602d474e0235f25b8df883a7e3db3db6fff8f86
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2022-03-16 15:45:59 +0530

    Day08

--- day08-misc-app/src/App.tsx
+++ day08-misc-app/src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Lab06 } from './lab06/Lab06';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BookDetails, BookList, Contact, Home } from './MyPages';
+
 
 function App() {
   return (
     <div className="App">
-      
+       <HashRouter>
+         <div>
+           <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/contact" element={<Contact/>}></Route>
+            <Route path="/books" element={<BookList/>}></Route>
+            <Route path="/bookdetails/:title" element={<BookDetails/>}></Route>
+           </Routes>
+         </div>
+       </HashRouter>      
     </div>
   );
 }
--- day08-misc-app/src/MyPages.tsx
+++ day08-misc-app/src/MyPages.tsx
@@ -1,18 +1,40 @@
+import { Link, useParams } from "react-router-dom";
+
+export const Menu = () => {
+    return (<nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/contact">Contact us</Link></li>
+            <li><Link to="/books">Library</Link></li>
+        </ul>
+    </nav>);
+}
 export const Home = () => {
-    return (<div>Welcome to home page</div>);
+    return (<div><Menu/> <br/>Welcome to home page</div>);
 };
 
 export const Contact = () => {
-    return (<div>You can reach me at xyz@mail.com</div>);
+    return (<div><Menu/> <br/>You can reach me at xyz@mail.com</div>);
 };
 
 export const BookList = () => {
     return (<div> 
+        <Menu/>
         <h4>Book list</h4>
         <ul>
-            <li>Ajax hacks</li>
-            <li>Being harsh</li>
-            <li>Programming Kotlin</li>
+            <li>Ajax hacks <Link to="/bookdetails/ajax">Details</Link></li>
+            <li>Being harsh<Link to="/bookdetails/harsh">Details</Link></li>
+            <li>Programming Kotlin<Link to="/bookdetails/Kotlin">Details</Link></li>
         </ul>
+    </div>);
+}
+
+export const BookDetails = () => {
+    let {title} = useParams();
+    return (<div>
+        <Menu/>
+        <br/>
+        <h2>Details for {title}</h2>
+        <h4>Name: {title}</h4>    
     </div>);
 }
